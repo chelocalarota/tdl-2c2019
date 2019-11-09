e621b7859e923677cb9e06a5457312585b261aee
smonsech <santiagomonsech@gmail.com>
2019-11-09 19:27:18 -0300

    Pycall ya no se buildea en cada corrida

--- examples/distributed_hello_world.jl
+++ examples/distributed_hello_world.jl
@@ -1,11 +1,11 @@
 using Distributed
 
-workervec = [("ubuntu@18.221.214.62:22", 1)]
+workervec = [("ubuntu@18.221.214.62:22", 2)]
 addprocs(workervec; sshflags=`-i ./JuliaServer.pem`, tunnel=true, exename=`/home/ubuntu/julia-1.2.0/bin/julia`)
 @everywhere using Distributed
-@everywhere import Pkg
-@everywhere Pkg.instantiate()
-@everywhere Pkg.add("PyCall")
+#@everywhere import Pkg
+#@everywhere Pkg.instantiate()
+#@everywhere Pkg.add("PyCall")
 @everywhere using PyCall
 @everywhere @pyimport socket
 println(workers())
@@ -13,7 +13,7 @@ println(workers())
 @everywhere function printhello(data)
 	println("hi from node ", myid())
 	println("my hostname is ", socket.gethostname())
-	println(" ann the data is ", data)
+	println(" and the data is ", data)
 end
 
 x = [@spawnat i printhello(i*10) for i in workers()]
