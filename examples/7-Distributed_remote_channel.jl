5e4d8b51643971675080a06f7eac8126af6eca79
smonsech <santiagomonsech@gmail.com>
2019-11-10 00:14:38 -0300

    remote

---
+++ examples/7-Distributed_remote_channel.jl
@@ -0,0 +1,40 @@
+using Distributed
+
+workervec = [("ubuntu@18.221.214.62:22", 2)]
+addprocs(workervec; sshflags=`-i ./JuliaServer.pem`, tunnel=true, exename=`/home/ubuntu/julia-1.2.0/bin/julia`)
+addprocs(2)
+@everywhere using Distributed
+@everywhere using PyCall
+@everywhere foo = pyimport("socket")
+println(workers())
+
+@everywhere @sync function listenChannel(channel)
+	println("Listen Started")
+	id = 1
+	while(id != -1)
+		id = take!(channel)
+		message = string("I'm process: ", string(myid()), " and i read element: ", id, " from the channel")
+		println(message)
+	end
+	println(string("i'm process: ", string(myid()), " and i'm finished listening"))
+end
+
+#Crea un canal remoto, para el pid 1(el main process) que alojara un canal vacio de 1000 Strings
+channel = RemoteChannel(() -> Channel{String}(1000), 1)
+x = [@spawnat i listenChannel(channel) for i in workers()]
+
+#pero ahora populo el canal para que los procesos extraigan del canal
+function fillChannel(channel, size)
+	for i=1:size
+		put!(channel, string("Elemento: ", size))
+	end
+
+	for i=1:length(workers())
+		put!(channel, string(-1))
+	end
+end
+
+println("filling the channel")
+fillChannel(channel, 4)
+#detener los procesos
+interrupt(workers())
